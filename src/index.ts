f26b4d90e11f6ac00541b27f47d1562139514cb5
Arthur Geron <arthur.geron@tc.com.br>
2022-07-21 19:48:21 -0300

    fix: ignore main export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import requireMemoRule from "./require-memo";
 import requireUseMemoRule from "./require-usememo";
 import requireUseMemoChildrenRule from "./require-usememo-children";
 
+// ts-prune-ignore-next
 export const rules = {
   "require-memo": requireMemoRule,
   "require-usememo": requireUseMemoRule,
