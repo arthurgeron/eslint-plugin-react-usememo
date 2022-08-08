889c70c36fab7846de442fe16506dd1265fd034e
Arthur Geron <arthurgeron@hotmail.com>
2022-08-08 12:34:02 -0300

    feat: enable coverage jest

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
   moduleNameMapper: {
     "src/(.*)": "<rootDir>/src/$1"
   },
+  testPathIgnorePatterns : [
+    "<rootDir>/src/index.ts", 
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/*',
+  ],
   cacheDirectory: '.jest-cache',
 };
