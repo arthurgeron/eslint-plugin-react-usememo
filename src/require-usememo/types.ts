7e2be6f0df0d3c34b81e612f8cfb9e49db85cfb9
Arthur Geron <arthurgeron@hotmail.com>
2024-01-20 13:07:52 -0300

    chore: removed unused type exports

--- src/require-usememo/types.ts
+++ src/require-usememo/types.ts
@@ -1,12 +1,10 @@
 import { Rule } from "eslint";
 import type { TSESTree } from "@typescript-eslint/types";
 import { MessagesRequireUseMemo} from '../constants';
-import type { ImportDeclaration } from "typescript";
 
 export type ExpressionTypes = TSESTree.ArrowFunctionExpression | TSESTree.JSXExpressionContainer | TSESTree.Expression | TSESTree.ObjectExpression | TSESTree.ArrayExpression | TSESTree.Identifier | TSESTree.LogicalExpression | TSESTree.JSXEmptyExpression;
 
 export type NodeType = TSESTree.MethodDefinitionComputedName;
-export type ESNode = TSESTree.CallExpression & Rule.NodeParentExtension
 export type ExpressionData = Record<string | number | symbol, keyof typeof MessagesRequireUseMemo>;
 type OptionalRecord<K extends keyof any, T> = {
   [P in K]?: T;
