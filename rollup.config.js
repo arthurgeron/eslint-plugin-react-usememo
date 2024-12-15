9745414eea05c4b1f7048707fdb9d2b71832a1ee
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-12-15 17:27:02 -0300

    feat: export on both cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
+import resolve from '@rollup/plugin-node-resolve';
 
-export default {
-  input: "src/index.ts",
-  output: [{ file: "dist/index.js", format: "cjs" }],
-  plugins: [resolve({ extensions: [".ts"] }), commonjs(), typescript()],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+    external: ['eslint', '@typescript-eslint/types'],
+  },
+];
